cf41a4992b294a734d88b3fdf863ec3b49355c38
stdarkpha <37771864+stdarkpha@users.noreply.github.com>
2021-09-14 14:51:43 +0700

    update

--- foot.php
+++ foot.php
@@ -19,7 +19,7 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4992063590043!2d106.84107331528021!3d-6.197675662443157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f46aeb819091%3A0x2e1f3a0e5ebb26!2sJl.%20Kimia%20No.20%2C%20RT.10%2FRW.1%2C%20Pegangsaan%2C%20Kec.%20Menteng%2C%20Kota%20Jakarta%20Pusat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2010320!5e0!3m2!1sid!2sid!4v1597938004844!5m2!1sid!2sid" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
             <div class="bottom">
-                &copy; Copyright Fikomubk | 2020 - Allright Reserve
+                &copy; Copyright StDPha | 2020 - Allright Reserve
             </div>
         </div>
     </footer></div>
